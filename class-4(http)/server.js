0d5c74724c5b7eceb9a62fd78b20456198771850
Samrudhips <20cs134.samrudhi@sjec.ac.in>
2024-12-16 18:44:16 +0530

    16/12/2024

--- class-4(http)/server.js
+++ class-4(http)/server.js
@@ -1,11 +1,23 @@
-import http from 'http'
-//create server
-let server=http.createServer((req,resp)=>{
-    resp.end("Hello,GM")
+// import http from 'http'
+// //create server
+// let server=http.createServer((req,resp)=>{
+//     resp.end("Hello,GM")
 
+// });
+// server.listen(8080,'127.0.0.1',(err)=>{
+//     if(err) throw err
+//     console.log(`Server is running on:http//localhost:${8080}`);
+    
+// })
+//In browser type 127.0.0.1:8080
+//Install npm i -g nodemon in cmd
+import http from 'http'
+let server=http.createServer((req,resp)=>{
+    resp.end("Hello,Hi,Good morning")
 });
 server.listen(8080,(err)=>{
     if(err) throw err
-    console.log(`Server is running on:http//localhost:${8080}`);
-    
+    console.log("Server running on port:8080")
+
 })
+//In browser give localhost:8080  
--- class-4.1(http2)/server.js
+++ class-4.1(http2)/server.js
@@ -1,15 +1,28 @@
-import http from 'http'
+// import http from 'http'
+// import fs from 'fs'
+// //create server
+// let server=http.createServer((req,resp)=>{
+//     fs.readFile('index.html','utf-8',(err,data)=>{
+//         if(err) throw err
+//         resp.end(data)
+//     })
+
+// });
+// server.listen(8080,(err)=>{
+//     if(err) throw err
+//     console.log(`Server is running on:http//localhost:${8080}`);
+// })
+//In browser type localhost:8080
 import fs from 'fs'
-//create server
+import http from 'http'
 let server=http.createServer((req,resp)=>{
     fs.readFile('index.html','utf-8',(err,data)=>{
         if(err) throw err
         resp.end(data)
     })
-
 });
 server.listen(8080,(err)=>{
-    if(err) throw err
+    if (err) throw err
     console.log(`Server is running on:http//localhost:${8080}`);
     
 })
