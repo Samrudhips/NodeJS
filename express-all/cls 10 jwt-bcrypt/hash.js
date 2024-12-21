fcabdc27ab357f4f5c980dbc3fced02e5c41eaa7
Samrudhips <20cs134.samrudhi@sjec.ac.in>
2024-12-21 19:28:25 +0530

    21/12/2024

---
+++ express-all/cls 10 jwt-bcrypt/hash.js
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt'
+let user={
+    'email':'rahul@gmail.com',
+    'cc':'12345678',
+    'cvv':'142'
+}
+let salt=bcrypt.genSaltSync(10);
+let new_cc=bcrypt.hashSync(user.cc,salt)
+let new_cvv=bcrypt.hashSync(user.cvv,salt)
+console.log(user.cc);
+console.log(new_cc);
+console.log(user.cvv);
+console.log(new_cvv);
+console.log(user);
+user={
+    ...user,cc:new_cc,cvv:new_cvv
+}
+console.log(user);
+
